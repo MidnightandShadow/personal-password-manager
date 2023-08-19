9369ac5ee12e96c6e4ba05af375def8a1afc32a3
Ivan Berlin <ivandberlin@gmail.com>
2023-08-19 19:49:13 -0400

    project planning

---
+++ Login/test_login.py
@@ -0,0 +1,7 @@
+# Logins are comprised of an email address (unique key) and a key. This key is the same "master key" for
+# access to all the accounts/passwords for the user.
+
+# Scenarios to test:
+# Valid email and password, invalid email, invalid password, missing email, missing password,
+# invalid email and password(? this would effectively be the same as invalid email, since that's the unique
+# key for users),
